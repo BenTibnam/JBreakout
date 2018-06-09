c9e3f9720b75ddfd263fc052698f9974c6760d18
BenTibnam <BenTibnam@users.noreply.github.com>
2018-06-09 11:39:53 -0400

    added icon to frame

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 public class Main {
-    public static void main(String[] args) {
+    public void run() {
         JFrame frame = new JFrame("Breakout");
+        ImageIcon img = new ImageIcon("ball.png");
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("ball.png")));
         Gameplay gameplay = new Gameplay();
         frame.setBounds(10, 10, 700, 600);
         frame.addWindowListener(new WindowAdapter() {
@@ -16,5 +18,9 @@ public class Main {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.getContentPane().add(gameplay);
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
     }
 }
